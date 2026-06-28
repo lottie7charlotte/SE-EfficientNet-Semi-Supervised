6f13ced309db341c0217fa46492602332d12d2ca
Liting Liao <lottie7.charlotte@gmail.com>
2026-06-28 16:58:13 +0800

    utils.py

--- utils.py
+++ utils.py
@@ -10,5 +10,5 @@ def set_seed(seed=42):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-        #torch.backends.cudnn.deterministic = True
-        #torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
